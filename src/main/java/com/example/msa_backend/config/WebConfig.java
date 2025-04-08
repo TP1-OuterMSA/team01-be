60f923876becb4a0a5ded15c50b8a1cd36325918
starshape77 <111961655+starshape7@users.noreply.github.com>
2025-04-08 12:00:55 +0900

    feat:수정사항 반영

--- src/main/java/com/example/msa_backend/config/WebConfig.java
+++ src/main/java/com/example/msa_backend/config/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173")  // Vite 프론트엔드 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용 메서드
-                .allowCredentials(true);  // 인증 정보(Cookie 등) 포함 허용
+        registry.addMapping("/**");  // 모든 경로에 대해
+//                .allowedOrigins("http://localhost:5173")  // Vite 프론트엔드 주소
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용 메서드
+//                .allowCredentials(true);  // 인증 정보(Cookie 등) 포함 허용
     }
 }
