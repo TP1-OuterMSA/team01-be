5db39ccb033b2405667389c763e3a714e9b78f43
추연우 <111961655+starshape7@users.noreply.github.com>
2025-04-27 11:14:35 +0900

    feat:webconfig수정 (#10)

--- src/main/java/com/example/msa_backend/config/WebConfig.java
+++ src/main/java/com/example/msa_backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해
-                .allowedOrigins("*")  // Vite 프론트엔드 주소
+                .allowedOriginPatterns("*")  // Vite 프론트엔드 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 인증 정보(Cookie 등) 포함 허용
