7bb19c1a7f8455b6b1a9b323d7b23222099594ad
JunSeo2001 <kanu6233@naver.com>
2025-05-11 18:45:10 +0900

    카프카, cors 설정 변경

--- src/main/java/com/example/msa_backend/config/WebConfig.java
+++ src/main/java/com/example/msa_backend/config/WebConfig.java
@@ -9,10 +9,24 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 경로에 대해
-                .allowedOriginPatterns("*")  // Vite 프론트엔드 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용 메서드
-                .allowedHeaders("*")  // 모든 헤더 허용
-                .allowCredentials(true);  // 인증 정보(Cookie 등) 포함 허용
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .exposedHeaders("Authorization", "Set-Cookie");
     }
 }
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")  // 모든 경로에 대해
+//                .allowedOrigins("http://localhost:5173")  // Vite 프론트엔드 주소
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용 메서드
+//                .allowedHeaders("*")  // 모든 헤더 허용
+//                .allowCredentials(true);  // 인증 정보(Cookie 등) 포함 허용
+//    }
+//}
