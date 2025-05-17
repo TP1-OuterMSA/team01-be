26f59405ee24347dc57bbeb10ed45c69430670fe
JunSeo2001 <kanu6233@naver.com>
2025-05-17 15:40:13 +0900

    kafka 메시지 받기

--- src/main/java/com/example/msa_backend/config/WebConfig.java
+++ src/main/java/com/example/msa_backend/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")  // Vite 프론트엔드 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
-                .allowCredentials(true);  // 인증 정보(Cookie 등) 포함 허용
+                .allowCredentials(false);  // 인증 정보(Cookie 등) 포함 허용
     }
 }
