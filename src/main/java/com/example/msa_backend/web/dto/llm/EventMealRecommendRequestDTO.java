f7996392efe4e65874abacd7488179d15051a1bd
starshape77 <111961655+starshape7@users.noreply.github.com>
2025-05-29 13:46:04 +0900

    FEAT: 운영 가이드 추가

--- src/main/java/com/example/msa_backend/web/dto/llm/EventMealRecommendRequestDTO.java
+++ src/main/java/com/example/msa_backend/web/dto/llm/EventMealRecommendRequestDTO.java
@@ -1,8 +1,11 @@
 package com.example.msa_backend.web.dto.llm;
 
+import com.example.msa_backend.domain.enums.MealType;
+import com.example.msa_backend.domain.enums.Weather;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class EventMealRecommendRequestDTO {
@@ -11,11 +14,11 @@ public class EventMealRecommendRequestDTO {
     @RequiredArgsConstructor
     public static class Request {
 
-        private final String date;
-        private final String mealType;
-        private final int count;
-        private final String weather;
-        private final int temperature;
+        private final LocalDate date;
+        private final MealType mealType;
+        private final Long count;
+        private final Weather weather;
+        private final double temperature;
         private final List<String> events;
     }
 }
--- src/main/java/com/example/msa_backend/web/dto/llm/EventMealRecommendResponseDTO.java
+++ src/main/java/com/example/msa_backend/web/dto/llm/EventMealRecommendResponseDTO.java
@@ -1,17 +1,31 @@
 package com.example.msa_backend.web.dto.llm;
 
+import com.example.msa_backend.domain.enums.MealType;
+import com.example.msa_backend.web.dto.manage.ManageResponseDTO;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
 public class EventMealRecommendResponseDTO {
-    private String recommendation;
 
-    public EventMealRecommendResponseDTO(String recommendation) {
-        this.recommendation = recommendation;
-    }
 
-    public String getRecommendation() {
-        return recommendation;
-    }
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    @Builder(access = AccessLevel.PRIVATE)
+    public static class RecommendResponseDTO {
+        private final LocalDate date;
+        private final Long people;
+        private final MealType mealType;
 
-    public void setRecommendation(String recommendation) {
-        this.recommendation = recommendation;
+        public static EventMealRecommendResponseDTO.RecommendResponseDTO toDTO(LocalDate date, Long people, MealType mealType) {
+            return EventMealRecommendResponseDTO.RecommendResponseDTO.builder()
+                    .date(date)
+                    .people(people)
+                    .mealType(mealType)
+                    .build();
+        }
     }
 }
